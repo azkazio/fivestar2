4d66978f9aa58843aaa2c4c633ba8137bac75146
ronny2404 <asroni.firdos24@gmail.com>
2026-04-15 11:14:25 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'v1_smart_cache';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './manifest.json'
+];
+
+// Install & Cache Assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Smart Cache Management (Hapus cache lama saat update)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Offline Mode: Stale-while-revalidate
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            const fetchPromise = fetch(event.request).then((networkResponse) => {
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, networkResponse.clone());
+                });
+                return networkResponse;
+            });
+            return cachedResponse || fetchPromise;
+        })
+    );
+});
