46fd3465ac907c0297b0c2cc6df350f5cef5de48
azkazio <asroni.firdos24@gmail.com>
2026-04-27 02:37:13 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+// sw.js
+
+// SETIAP KALI UPDATE SKRIP, GANTI ANGKA DI BAWAH (misal 1.1 ke 1.2)
+const VERSION = '1'; 
+const CACHE_NAME = `fs2-cache-${VERSION}`;
+
+// 1. Proses Install
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Paksa versi baru langsung standby
+    console.log(`SW: Versi ${VERSION} terpasang`);
+});
+
+// 2. Proses Aktivasi & Pembersihan Cache Lama
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('SW: Menghapus Cache Lama:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// 3. Menangkap Perintah dari UI
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
+
+// 4. Pengaturan Fetch (Agar aplikasi kencang)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
